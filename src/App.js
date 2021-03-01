5c9b07f306e1e3d6405865fc77442f3cde6eb664
Olga Ivanova <o.ivanova@viamedici.de>
2021-03-01 18:18:43 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FeatureImage from './FeatureImage'
+import Movies from './Movies'
+
+const App = () => {
+  return (
+    <div>
+      <FeatureImage />
+      <Movies />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FeatureImage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FeatureImage = () => {
+  return (
+    <div>
+      Feature Image
+    </div>
+  );
+}
+
+export default FeatureImage;
